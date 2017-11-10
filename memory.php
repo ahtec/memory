7bc318f8832b2ceb654b9f6b1255c556fca22351
ahtec <gerarddoets@gmail.com>
2017-11-10 13:08:04 +0100

    memory plaatjes worden gematched

--- memory.php
+++ memory.php
@@ -10,7 +10,7 @@
 //                alert(aantalKlikken);
                 if (aantalKlikken % 2 == 0) {
 //                    alert("even");
-                    vorgePlaatje = document.getElementById("duck" + vorigeKlik).src;
+                    vorgePlaatje  = document.getElementById("duck" + vorigeKlik).src;
                     huidigPlaatje = document.getElementById("duck" + teller).src;
                     if (vorgePlaatje == huidigPlaatje) {
                         alert("zelfde plaatje");
@@ -39,18 +39,28 @@
                 $eenden = array();
                 $eenden[] = "eend1.jpg";
                 $eenden[] = "eend2.jpg";
-                $eenden[] = "eend1.jpg";
-                $eenden[] = "eend2.jpg";
+                $eenden[] = "eend3.jpg";
+                $eenden[] = "eend4.jpg";
                 $eenden[] = "eend5.jpg";
                 $eenden[] = "eend6.jpg";
                 $eenden[] = "eend7.jpg";
                 $eenden[] = "eend8.jpg";
+                $eenden[] = "eend1.jpg";
+                $eenden[] = "eend2.jpg";
+                $eenden[] = "eend3.jpg";
+                $eenden[] = "eend4.jpg";
+                $eenden[] = "eend5.jpg";
+                $eenden[] = "eend6.jpg";
+                $eenden[] = "eend7.jpg";
+                $eenden[] = "eend8.jpg";
+                
 //                var_dump($eenden);
                 $i = 0;
-                for ($x = 1; $x < 3; $x++) {
-                    for ($y = 1; $y < 3; $y++) {
+                for ($x = 1; $x < 5; $x++) {
+                    for ($y = 1; $y < 5; $y++) {
                         
-                        echo "\n<td>    <p>  <img onclick = 'verander($x$y)' src='".$eenden[$i]."' width=210 height=210 id=duck$x$y /> </p> </td>";
+                        echo "\n<td>    <p>  <img onclick = 'verander($x$y)' src='".$eenden[$i]."' width=50 height=50 id=duck$x$y /> </p> </td>";
+//                        echo "\n<td>    <p>  <img onclick = 'verander($x$y)' src='".$eenden[$i]."' width=50 height=50 id=duck$x$y /> </p> </td>";
                     $i++;
                         
                     }

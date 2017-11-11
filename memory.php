1bfe95f6fa3cca25b95d5bec959ba89ecbf8f3fb
ahtec <gerarddoets@gmail.com>
2017-11-11 12:44:00 +0100

    bezig met timeoput of sttus complete last picture problem

--- memory.php
+++ memory.php
@@ -25,13 +25,20 @@
             var succesvolGrdraaid = [];
 
             var succes = false;
+            function wachten(){
+             alert("wachten")   
+            }
 
+            function loaded() {
+//                alert('loaded')
+            }
 
             function flip(teller) {
-
-//                if (succes)
-//                    alert("succes");
-
+//
+//               if (succes){
+//var x = document.getElementById("melding");
+//x.innerHTML = "aksjdhfkjahskfj"
+//            }
                 //                alert(eenden[teller]);
                 console.log(teller);
 //                console.log(eenden[teller]);
@@ -73,16 +80,38 @@
                     //                    alert(eenden[teller]);
                     document.getElementById("duck" + teller).src = eenden[teller];
                 }
-
+//document.write("HHHHHHHHHHHHHHHHHHH");
                 if (succesvolGrdraaid.length >= eenden.length) {
-//                    succes = true;
 //                    alert("Klaar");
-//                    document.getElementById("duck" + teller).src = eenden[teller];
+                    document.getElementById("duck" + teller).src = eenden[teller];
+                    var laatste = document.getElementById("duck" + teller)
+                    setTimeout('wachten()',   3000);
+
+                    if (laatste.complete) {
+                        loaded()
+                    } else {
+                        laatste.addEventListener('load', loaded)
+                        laatste.addEventListener('error', function () {
+                            alert('error')
+                        })
+                    }
+
+//                    var tussen = document.getElementById("duck" + teller);
+
+//                    document.write("Gefeliciteerd, u heeft een goed geheugen,   ");
+
 //                    location.reload();
 //                   document.reload();
+
+
+
                     console.log(teller);
                     if (aantalKlikken - eenden.length < 20) {
-                        alert("Gefeliciteerd, u heeft een goed geheugen,   u heeft er " + aantalKlikken + " klikken over gedaan. dat is geweldig goed,\nNu beginnen we overnieuw maar nu moeilijker")
+
+
+                        var meldingText = "Gefeliciteerd, u heeft een goed geheugen,   u heeft er " + aantalKlikken + " klikken over gedaan. dat is geweldig goed,\nNu beginnen we overnieuw maar nu moeilijker";
+                        var x = document.getElementById("melding");
+                        x.innerHTML = meldingText;
                         for (i = 0; i < eenden.length; i++) {
                             //                        eenden.forEach( zetOpZwart() ze)
                             document.getElementById("duck" + i).src = 'zwart.png';
@@ -96,6 +125,7 @@
                             document.getElementById("duck" + i).src = 'zwart.png';
                         }
                     }
+                    succes = true;
                     succesvolGrdraaid.length = 0;
                     aantalKlikken = 0;
                 }
@@ -124,11 +154,13 @@
                 echo "<tr>";
                 for ($y = 1; $y < 5; $y++) {
                     echo "\n<td>    <p>  <img onclick = 'flip($i)' src='zwart.png' width=120 height=120 id=duck$i /> </p> </td>";
+                    //echo "\n<td> $x$y   <p>  <button onclick = 'flip($i)'  id=duck$i /> </p> </td>";
                     $i++;
                 }
                 echo " </tr> ";
             }
             ?>
+            <div id=melding> Memory </div>
 
 
 
